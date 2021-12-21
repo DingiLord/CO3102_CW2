090536ebf04444597e715505099f2a545048357e
Lukas Krasauskis <lukas.krasauskis123@gmail.com>
2021-12-21 21:04:48 +0000

    Added a function to get data from the firebase and add the questions to the screen.

--- app/src/main/java/com/example/co3102_cw2/AdminActivity.java
+++ app/src/main/java/com/example/co3102_cw2/AdminActivity.java
@@ -1,19 +1,30 @@
 package com.example.co3102_cw2;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.co3102_cw2.Adapter.QuestionAdapter;
 import com.example.co3102_cw2.Model.QuestionListItem;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -27,7 +38,6 @@ public class AdminActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
         getSupportActionBar().hide();
-        questionList = new ArrayList<>();
         questionRecyclerView = findViewById(R.id.QuestionsRecyclerViewAdmin);
         questionRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         questionAdapter = new QuestionAdapter(this);
@@ -39,19 +49,54 @@ public class AdminActivity extends AppCompatActivity {
         item.setQuestion("What colour is the sky?");
         item.setStatus(false);
 
-        questionList.add(item);
-        questionList.add(item);
-        questionList.add(item);
-        questionList.add(item);
-        questionList.add(item);
+        InitialData(questionAdapter);
 
-        questionAdapter.setQuestionList(questionList);
+//        questionList.add(item);
+//        questionList.add(item);
+//        questionList.add(item);
+//        questionList.add(item);
+//        questionList.add(item);
+
+
 
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getBaseContext(),AddNewQuestionActivity.class);
                 startActivity(intent);
+            }
+        });
+
+    }
+
+    // Populates Questions from the Database
+    public void InitialData(QuestionAdapter questionAdapter){
+        questionList = new ArrayList<>();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("general").document("Questions");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    if(documentSnapshot.exists()){
+                        for(Map.Entry<String,Object> i : documentSnapshot.getData().entrySet()){
+                          Log.d(TAG,"Question?: "+i.getValue().toString());
+                          QuestionListItem item = new QuestionListItem();
+                          item.setStatus(false);
+                          item.setQuestion(i.getValue().toString());
+                          questionList.add(item);
+                        }
+                        questionAdapter.setQuestionList(questionList);
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                //TODO: action if it fails
             }
         });
 
